7b023d4d87baf1bdd8b16be137d8d31b852e58bb
loryruta <loryruta23@gmail.com>
2026-03-28 01:06:19 +0000

    Update

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,20 @@
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets");
+
+    // Nunjucks helper returning the current year (used in templates)
+    eleventyConfig.addNunjucksGlobal("now", function () {
+        return new Date();
+    });
+
+    eleventyConfig.addNunjucksGlobal("getAge", function () {
+        const today = new Date();
+        const birthDate = new Date("2000-08-23");
+        let age = today.getFullYear() - birthDate.getFullYear();
+        const m = today.getMonth() - birthDate.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+        return age;
+    });
 };
